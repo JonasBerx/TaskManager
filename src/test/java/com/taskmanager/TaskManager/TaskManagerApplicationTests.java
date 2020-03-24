0bab5a46df2b538c53c128f0f16f9f5ab97a857e
JonasBerx <jonas.berx1@student.ucll.be>
2020-03-24 16:51:03 +0100

    trying to fix but idk wtf is wrong

--- src/test/java/com/taskmanager/TaskManager/TaskManagerApplicationTests.java
+++ src/test/java/com/taskmanager/TaskManager/TaskManagerApplicationTests.java
@@ -4,13 +4,15 @@ import be.ucll.Taskmanager.DB.TaskService;
 import be.ucll.Taskmanager.Domain.Task;
 import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class TaskManagerApplicationTests {
 
